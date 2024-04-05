2634c21a562761dd4edad0445681aefc4cef7f20
naresh <maddurinaresh7@gmail.com>
2024-04-05 12:16:35 +0530

    pulled lsatest code

--- src/components/about/Aboutbanner.jsx
+++ src/components/about/Aboutbanner.jsx
@@ -9,14 +9,11 @@ const Aboutbanner = () => {
         <div className="about-us-banner-background">
             <div className="row">
                 <div className="col-md-5 about-us-banner-left-section">
-                    <h5 className='about-us-banner-subtitle'>About Us</h5>
-                    <h1 className='about-us-banner-title'>House Construct: Crafting Excellence Since 1997</h1>
-                    <p className='about-us-banner-description'>Join us in our mission to redefine the concept of home. Discover the difference that experience, expertise, and passion can make in creating living spaces that are nothing short of exceptional.</p>
-                </div>
-                <div className="col-md-7">
-                    <div className='about-us-banner-image-parent'>
-                        <img className='about-us-banner-image' src="/images/about-us-banner-image.webp" alt="" />
+                    <div className="about-us-banner-subtitle d-flex gap-2">
+                        <p>Home</p>
+                        <p>. About Us</p>
                     </div>
+                    <h1 className='about-us-banner-title'>About Us</h1>
                 </div>
             </div>
         </div>
